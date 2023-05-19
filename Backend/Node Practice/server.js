ae8b6191db683d389cb5f10e273790861f4d8e79
Khaledr001 <khaledrazu01@gmail.com>
2023-05-19 21:30:50 +0600

    node boilerplate added

---
+++ Backend/Node Practice/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
