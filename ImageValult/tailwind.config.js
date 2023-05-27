26dccf0ca7db1b0277d9f9e143fc2d80d7294598
Khaledr001 <khaledrazu01@gmail.com>
2023-05-27 13:56:23 +0600

    ImageVault Complete

---
+++ ImageValult/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      "navbarColor": "#232228",
+      "sidebarColor": "#35353c",
+      "bodyColor": "#3c3c43",
+      "textColor": "#fff",
+    },
+  },
+    
+  },
+  plugins: [],
+}
--- Node Practice/server.js
+++ Node Practice/server.js
@@ -1,10 +1,79 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 
-const port = 3000;
+
+// API methods
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API!'
+  })
+})
+
+let users = [];
+let lastId = 0;
+
+// API to create a new user
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  user.id = lastId++;
+  users.push(user);
+  res.json(user);
+})
+
+// API to update an user
+
+app.put('/userss/:id', (req, res) => {
+  const id = req.params.id;
+  const bodies = req.body;
+
+  const user = users.find(user => user.id == id);
+  if (user) {
+    user.fname = bodies.fname;
+    user.lname = bodies.lname;
+    res.json(user);
+  }
+  else {
+    res.status(404).json({message: "user not found"});
+  }
+});
+
+// API to delete an user
+
+app.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const userIndex = users.findIndex(user => user.id == id);
+  if (userIndex > -1) {
+      // users = users.filter(user => user.id!= id);
+      user.splice(userIndex, 1);
+      res.json(user);
+    }
+  else {
+      res.status(404).json({message: "user not found"});
+    }
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => user.id == id);
+  if(user) {
+    res.json(user);
+  }
+  else {
+      res.sendStatus(404).json({message: "user not found"});
+    }
+})
+
+const port = 6000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
