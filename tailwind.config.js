1a9f170c2ac71e288e15bd2f988d608c0470930a
Khaledr001 <khaledrazu01@gmail.com>
2023-06-08 22:07:08 +0600

    Image Vault Complete

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "navbarColor": "#232228",
+        "sidebarColor": "#35353c",
+        "bodyColor" : "#3c3c43",
+
+      }
+    },
+  },
+  plugins: [],
+}
